2911c121dc5116a406fa6aed2a322d9e5a681a9c
Marvellous <jeyuso1104@gmail.com>
2024-11-25 14:45:32 +0000

    pu

--- practice.js
+++ practice.js
@@ -2,3 +2,12 @@ document.getElementById("hamburger").addEventListener("click",function() {
      const nav= document.querySelector(".nav");
      nav.classList.toggle("active");
 });
+
+
+
+
+
+
+
+
+
